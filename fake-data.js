da05871ba9e12732d45e3381bdd2a9ed94c6b129
uchihaNayeem <uchiha.nayeem@gmail.com>
2020-08-27 16:12:05 +0600

    again testing json

--- fake-data.js
+++ fake-data.js
@@ -1,4 +1,4 @@
-[
+[ {
   {
     "id": "1",
     "name": "React Course 2020",
@@ -6,9 +6,10 @@
     "img": "https://i.morioh.com/2019/10/23/620a990fe531.jpg"
   },
   {
-    "id": "1",
+    "id": "2",
     "name": "React Course 2020",
     "price": "501",
     "img": "https://i.morioh.com/2019/10/23/620a990fe531.jpg"
-  }
+  },
+}
 ]
